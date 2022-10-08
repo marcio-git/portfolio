aae23e5b3459ed9ba2e9eda7370f4e2bb1b8cc87
marcio-git <marcio.dev.work@gmail.com>
2022-10-08 19:15:25 -0300

    Nav component -> small changes

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -12,11 +12,13 @@ const MenuItem = ({open, ...rootDOMAttributes}) => {
 }
 
 const NavItem = ({ item, link, isOpen, ...rootDOMAttributes }) => {
-  return <li className={styles.nav_item} {...rootDOMAttributes}>
-    <a href={link} className={styles.nav_link}>
-      <span className={styles.link_text}>{item}</span>
-    </a>
-  </li>
+  return (
+    <li className={styles.nav_item} {...rootDOMAttributes}>
+      <a href={link} className={styles.nav_link}>
+        <span className={styles.link_text}>{item}</span>
+      </a>
+    </li>
+  )
 }
 
 export default function NavBar() {
@@ -28,19 +30,19 @@ export default function NavBar() {
         <NavItem
           item={'HOME'}
           link={'#home'}
-          onClick={() => setIsOpen(!isOpen)} />
+          onClick={() => setIsOpen(false)} />
         <NavItem
           item={'ABOUT ME'}
           link={'#about'}
-          onClick={() => setIsOpen(!isOpen)} />
+          onClick={() => setIsOpen(false)} />
         <NavItem
           item={'TECHNOLOGIES'}
           link={'#techno'}
-          onClick={() => setIsOpen(!isOpen)} />
+          onClick={() => setIsOpen(false)} />
         <NavItem
           item={'PORTFOLIO'}
           link={'#portfolio'}
-          onClick={() => setIsOpen(!isOpen)} />
+          onClick={() => setIsOpen(false)} />
       </ul>
       <MenuItem 
         open={isOpen}
