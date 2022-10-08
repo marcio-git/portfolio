0c80c48107481ca62649209fd377f05dd299eb25
marcio-git <marcio.dev.work@gmail.com>
2022-10-08 19:31:02 -0300

    Home component -> CV link updated

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -11,7 +11,7 @@ export default function Home() {
         <h1 className={styles.hidden}>HI THERE</h1>
         <h2 className={styles.hidden}>I'm Marcio</h2>
         <h3 className={styles.hidden}>a Full Stack Web Developer</h3>
-      <a target="_blank" href='https://drive.google.com/file/d/1-83BP3JrQT6GBAJDQdAzEPAftjMVDH5f' className={`${styles.button_33} ${styles.hidden}`} >Download CV</a>
+      <a target="_blank" href='https://drive.google.com/file/d/1xbvsoEDE4DZMUaGlF8dTmldYCGzE24tZ/view?usp=sharing' className={`${styles.button_33} ${styles.hidden}`} >Download CV</a>
       </div>
     </section>
   )
