2d3363287be25854fd6e7ea51b8ec771b57f70f6
marcio-git <marcio.dev.work@gmail.com>
2022-10-08 21:12:35 -0300

    Home component -> icon added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,5 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
+import { Icon } from '@iconify/react';
 import { Obs } from '../App';
 import styles from './Home.module.css';
 
@@ -11,7 +12,9 @@ export default function Home() {
         <h1 className={styles.hidden}>HI THERE</h1>
         <h2 className={styles.hidden}>I'm Marcio</h2>
         <h3 className={styles.hidden}>a Full Stack Web Developer</h3>
-      <a target="_blank" href='https://drive.google.com/file/d/1xbvsoEDE4DZMUaGlF8dTmldYCGzE24tZ/view?usp=sharing' className={`${styles.button_33} ${styles.hidden}`} >Download CV</a>
+      <a target="_blank" href='https://drive.google.com/file/d/1xbvsoEDE4DZMUaGlF8dTmldYCGzE24tZ/view?usp=sharing' className={`${styles.button_33} ${styles.hidden}`} >
+        <Icon icon="heroicons-outline:document-download" inline={true} />
+        Download CV</a>
       </div>
     </section>
   )
