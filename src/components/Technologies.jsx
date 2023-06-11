5f987f8b689249bce15c5cae5bf8265c420203aa
marcio-git <marcio.dev.work@gmail.com>
2023-06-11 03:18:13 -0300

    change: technologies component + techno.module

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -1,23 +1,25 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import { Icon } from '@iconify/react';
 import { Obs } from '../App';
 import styles from './Techno.module.css';
-import html_icon from '../assets/html_icon.png'
-import css_icon from '../assets/css_icon.png'
-import js_icon from '../assets/js_icon.png'
-import jquery_icon from '../assets/jquery_icon.png'
-import react_icon from '../assets/react_icon.png'
-import node_icon from '../assets/node_icon.png'
-import ex_icon from '../assets/ex_icon.png'
-import axios_icon from '../assets/axios_icon.png'
-import mysql_icon from '../assets/mysql_icon.png'
 
-const BoxItem = ({item, img, experience}) => {
-  return (
-    <div className={`${styles.box} ${styles.hidden}`}>
-      <p>{item}</p>
+const BoxItem = ({ icon, experience }) => {
+  const [click, setClick] = useState(false)
+  return (<>
+    <div className={`${styles.box} ${styles.hidden}`}
+      onClick={() => setClick(!click)}>
+      <div className={`${click ? styles.clicked : styles.container}`}>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span className={`${styles.fas} ${styles.fa_bolt}`} id="content">
+          <Icon icon={icon} />
+        </span>
+      </div>
       <p>Experience: {experience}</p>
-      <img src={img} alt="imagen" />
     </div>
+  </>
   )
 }
 export default function Technologies() {
@@ -26,49 +28,40 @@ export default function Technologies() {
   return (
     <section id='techno' className={styles.three}>
       <div className={styles.main_text}>
-        <h2>My Abilities:</h2>
+        <h2>Technical Skills:</h2>
       </div>
       <div className={styles.table}>
-        <BoxItem 
-          item='HTML'
-          img={html_icon}
-          experience={'2 years'}/>
-        <BoxItem 
-          item='CSS'
-          img={css_icon}
-          experience={'2 years'}/>
-        <BoxItem 
-          item='JavaScript'
-          img={js_icon}
-          experience={'2 years'}/>
-        <BoxItem 
-          item='JQuery'
-          img={jquery_icon}
-          experience={'1 year'}/>
-        <BoxItem 
-          item='ReactJS'
-          img={react_icon}
-          experience={'1 year'}/>
-        <BoxItem 
-          item='NodeJS'
-          img={node_icon}
-          experience={'6 months'}/>
-        <BoxItem 
-          item='Express'
-          img={ex_icon}
-          experience={'6 months'}/>
-        <BoxItem 
-          item='Axios'
-          img={axios_icon}
-          experience={'6 months'}/>
-        <BoxItem 
-          item='MySQL'
-          img={mysql_icon}
-          experience={'1 year'}/>
+        <BoxItem
+          icon={"logos:html-5"}
+          experience={'2 years'} />
+        <BoxItem
+          icon={"devicon:css3-wordmark"}
+          experience={'2 years'} />
+        <BoxItem
+          icon={"vscode-icons:file-type-js-official"}
+          experience={'2 years'} />
+        <BoxItem
+          icon={"devicon:jquery-wordmark"}
+          experience={'1 year'} />
+        <BoxItem
+          icon={"devicon:react-wordmark"}
+          experience={'1 year'} />
+        <BoxItem
+          icon={"logos:vitejs"}
+          experience={'1 year'} />
+        <BoxItem
+          icon={"logos:nodejs"}
+          experience={'6 months'} />
+        <BoxItem
+          icon={"devicon:express"}
+          experience={'6 months'} />
+        <BoxItem
+          icon={"logos:axios"}
+          experience={'6 months'} />
+        <BoxItem
+          icon={"logos:mysql"}
+          experience={'1 year'} />
       </div>
-      {/* <div className={styles.table}>
-      </div> */}
-      {/* <img className={styles.main_image} src=''></img> */}
     </section>
   )
 }
